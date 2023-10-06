0a9e56d2a22518f4c3b4f44c17b81af38536c1d6
TrieuDan <crazysub2002@gmail.com>
2023-10-07 01:00:52 +0700

    Phần cmt trong  getUserLocation là thử cách get khác đơn giản hơn, nhưng cả 2 cách đều trả về kết quả không nhận đc phản hồi từ api. Phúc check thử xem, Đan check 1 lần thấy khá chuẩn về mặt logic và cú pháp rồi.

--- app/src/main/java/com/example/iotapp/HomeActivity.java
+++ app/src/main/java/com/example/iotapp/HomeActivity.java
@@ -15,17 +15,22 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,9 +43,9 @@ public class HomeActivity extends AppCompatActivity {
     private FloatingActionButton fab;
     private BottomNavigationView navigationView;
     private static final int GALLERY_ADD_POST = 2;
-    private ProgressDialog dialog;
+    private RequestQueue requestQueue;
     private TextView currentLocation;
-    private  TextView currentUserName;
+    private TextView currentUserName;
     private float currentTemp;
     private float currentHumid;
 
@@ -51,79 +56,72 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_home);
         fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frameHomeContainer,new HomeFragment(),HomeFragment.class.getSimpleName()).commit();
+        fragmentManager.beginTransaction().replace(R.id.frameHomeContainer, new HomeFragment(), HomeFragment.class.getSimpleName()).commit();
         init();
     }
 
     private void init() {
         navigationView = findViewById(R.id.bottom_nav);
         fab = findViewById(R.id.fab);
-        fab.setOnClickListener(v->{
+        fab.setOnClickListener(v -> {
             Intent i = new Intent(Intent.ACTION_PICK);
             i.setType("image/*");
-            //startActivityForResult(i,GALLERY_ADD_POST);
+            startActivityForResult(i, GALLERY_ADD_POST);
         });
-        getUserInfoLocation();
-//
-//        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//
-//                switch (item.getItemId()){
-//                    case R.id.item_home: {
-//                        Fragment account = fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName());
-//                        if (account!=null){
-//                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName())).commit();
-//                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName())).commit();
-//                        }
-//                        break;
-//                    }
-//
-//                    case R.id.item_account: {
-//                        Fragment account = fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName());
-//                        fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName())).commit();
-//                        if (account!=null){
-//                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag(AccountFragment.class.getSimpleName())).commit();
-//                        }
-//                        else {
-//                            fragmentManager.beginTransaction().add(R.id.frameHomeContainer,new AccountFragment(),AccountFragment.class.getSimpleName()).commit();
-//                        }
-//                        break;
-//                    }
-//                }
-//
-//                return  true;
-//            }
-//        });
-//
-}
+        currentLocation = findViewById(R.id.txtLocation);
+        getUserLocation();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==GALLERY_ADD_POST && resultCode==RESULT_OK){
+        if (requestCode == GALLERY_ADD_POST && resultCode == RESULT_OK) {
             Uri imgUri = data.getData();
-            Intent i = new Intent(HomeActivity.this,AddActivity.class);
+            Intent i = new Intent(HomeActivity.this, AddActivity.class);
             i.setData(imgUri);
             startActivity(i);
         }
     }
 
 
-    private void getUserInfoLocation() {
-        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
-        StringRequest request = new StringRequest(Request.Method.POST, Constant.PICK_LOCATION, response -> {
+    private void getUserLocation() {
+//        requestQueue = Volley.newRequestQueue(this);
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, Constant.PICK_LOCATION, null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        try {
+//                            JSONArray jsonArray = response.getJSONArray("locations");
+//                            JSONObject jsonObject = jsonArray.getJSONObject(1);
+//                            String nameLocation = jsonObject.getString("name");
+//                            currentLocation.setText(nameLocation);
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                error.printStackTrace();
+//                currentLocation.setText("Error input location");
+//            }
+//        });
+//        requestQueue.add(request);
+//    }
+
+        StringRequest request = new StringRequest(Request.Method.GET, Constant.PICK_LOCATION, response -> {
             //we get response if connection success
             try {
                 JSONObject object = new JSONObject(response);
                 if (object.getBoolean("success")) {
-                    JSONObject user = object.getJSONObject("location");
+                    JSONObject user = object.getJSONObject("locations");
                     //make shared preference user
-                    SharedPreferences userPref = this.getApplicationContext().getSharedPreferences("location",  MODE_PRIVATE);
+                    SharedPreferences userPref = getSharedPreferences("locations", MODE_PRIVATE);
                     SharedPreferences.Editor editor = userPref.edit();
-                    editor.putString("name", object.getString("name"));
-                    editor.putInt("id", user.getInt("id"));
+                    editor.putString("name", user.getString("name"));
                     editor.apply();
+                    String nameLocation = user.getString("name");
+                    currentLocation.setText(nameLocation);
 
                     //if success
                     Toast.makeText(this, "Get info successed", Toast.LENGTH_SHORT).show();
@@ -131,13 +129,12 @@ public class HomeActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            },error -> {
-                // error if connection not success
-                error.printStackTrace();
-                Toast.makeText(this, "Get info failed", Toast.LENGTH_SHORT).show();
-        }){
-
-        };
+        }, error -> {
+            // error if connection not success
+            error.printStackTrace();
+            currentLocation.setText("Nowhere");
+            Toast.makeText(this, "Get info failed", Toast.LENGTH_SHORT).show();
+        });
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
     }
